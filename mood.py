bdb5074b6739ca8662fcd98681ff3fa683eb802b
Rimachan4 <rimanoble2004@gmail.com>
2025-02-07 01:36:58 +0530

    playlist songs pagination

--- mood.py
+++ mood.py
@@ -106,21 +106,33 @@ def get_current_user_playlists():
     if datetime.now().timestamp() > session['expires_at']:
         return redirect('/refresh')
     
-    type="tracks"
+    
     url=f"https://api.spotify.com/v1/me/playlists"
     
     header={"Authorization":"Bearer "+ session['access_token']}
     response=get(url,headers=header)
     result=response.json()
     
-    #result=jsonify(result_first['items'][0]['artists'][0]['name'])
-    data=session['data']
     for i in result['items']:
-        track_id=i['id']
+        playlist_id=i['id']
         
-        data.append({'track_id':track_id})    
-        
-    session['data']=data      
+    tracks_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"    
+    
+    data= session['data']
+    
+    #idk why i get an error in line 129 its a key error pls check the json to extract correctly
+    while tracks_url:
+        tracks_response=get(tracks_url,headers=header)
+        tracks_data=tracks_response.json()
+    
+        for j in tracks_data['items']:
+            track_id=j['id']
+            track_name = j['name']
+            track_artist = j['artists'][0]['name']
+            
+            data.append({'track_id': track_id, 'track_name': track_name, 'track_artist': track_artist})    
+        tracks_url=tracks_data.get('next')
+    session['data']=data     
     return redirect('/liked_songs')
 
 @app.route('/liked_songs')
